1b4399e29cb27b20f7f7eb6cbf873ccb63762058
Lucas-191435 <lucasfernandosantos2001@gmail.com>
2024-08-26 09:00:42 -0300

    modificacao schema database

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,4 @@
-import { Router } from 'express';
 import AccountRoutes from "../modules/account/account.routes";
-const router = Router();
-
-// Defina suas rotas aqui
-// router.get('/example', (req, res) => {
-//   res.send('Example route');
-// });
 
 export default [
   AccountRoutes,
