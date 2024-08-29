3a4d68077e06e0e7e7592199e1f8d923096d89a1
Lucas-191435 <lucasfernandosantos2001@gmail.com>
2024-08-29 11:17:17 -0300

    add .env example

--- src/provider/EazyProvider.ts
+++ src/provider/EazyProvider.ts
@@ -1,75 +1,75 @@
-import axios, { AxiosInstance } from "axios";
-import { Environment } from "../utils/Environment";
-// import { Eazy } from "../types/Eazy";
+// import axios, { AxiosInstance } from "axios";
+// import { Environment } from "../utils/Environment";
+// // import { Eazy } from "../types/Eazy";
 
-class EazyProvider {
-  public instance: AxiosInstance;
+// class EazyProvider {
+//   public instance: AxiosInstance;
 
-  private token: string | null;
+//   private token: string | null;
 
-  constructor() {
-    this.token = null;
+//   constructor() {
+//     this.token = null;
 
-    this.instance = axios.create({
-      baseURL: Environment.EazyBaseUrl,
-    });
+//     this.instance = axios.create({
+//       baseURL: Environment.EazyBaseUrl,
+//     });
 
-    this.instance.interceptors.request.use(async (config) => {
-      // Se ainda não tiver um token ou o token expirou, obtém um novo token
-      if (!this.token) {
-        this.token = await this.getNewAccessToken();
-      }
+//     this.instance.interceptors.request.use(async (config) => {
+//       // Se ainda não tiver um token ou o token expirou, obtém um novo token
+//       if (!this.token) {
+//         this.token = await this.getNewAccessToken();
+//       }
 
-      // Adiciona o token de acesso ao cabeçalho 'Authorization'
+//       // Adiciona o token de acesso ao cabeçalho 'Authorization'
 
-      // eslint-disable-next-line no-param-reassign
-      config.headers.Authorization = `Bearer ${this.token}`;
+//       // eslint-disable-next-line no-param-reassign
+//       config.headers.Authorization = `Bearer ${this.token}`;
 
-      return config;
-    });
+//       return config;
+//     });
 
-    this.instance.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        try {
-          const prevRequest = error?.config;
-          if (
-            (error?.response?.status === 401 ||
-              error?.response?.status === 403) &&
-            !prevRequest?.sent
-          ) {
-            prevRequest.sent = true;
-            this.token = await this.getNewAccessToken();
+//     this.instance.interceptors.response.use(
+//       (response) => response,
+//       async (error) => {
+//         try {
+//           const prevRequest = error?.config;
+//           if (
+//             (error?.response?.status === 401 ||
+//               error?.response?.status === 403) &&
+//             !prevRequest?.sent
+//           ) {
+//             prevRequest.sent = true;
+//             this.token = await this.getNewAccessToken();
 
-            if (!this.token) throw new Error("Erro ao atualizar sessão!");
+//             if (!this.token) throw new Error("Erro ao atualizar sessão!");
 
-            this.instance.defaults.headers.common.Authorization = `Bearer ${this.token}`;
+//             this.instance.defaults.headers.common.Authorization = `Bearer ${this.token}`;
 
-            return this.instance(prevRequest);
-          }
+//             return this.instance(prevRequest);
+//           }
 
-          return Promise.reject(error);
-        } catch (error) {
-          return Promise.reject(error);
-        }
-      }
-    );
-  }
+//           return Promise.reject(error);
+//         } catch (error) {
+//           return Promise.reject(error);
+//         }
+//       }
+//     );
+//   }
 
-  private getNewAccessToken = async () => {
-    const response = await axios.post<Eazy.Api.Login.Response>(
-      `${Environment.EazyBaseUrl}/organizations/login`,
-      Environment.EazyCredentials
-    );
+//   private getNewAccessToken = async () => {
+//     const response = await axios.post<Eazy.Api.Login.Response>(
+//       `${Environment.EazyBaseUrl}/organizations/login`,
+//       Environment.EazyCredentials
+//     );
 
-    return response.data.token;
-  };
+//     return response.data.token;
+//   };
 
-  public get apiInstance() {
-    return this.instance;
-  }
-}
+//   public get apiInstance() {
+//     return this.instance;
+//   }
+// }
 
-const eazyProvider = new EazyProvider();
+// const eazyProvider = new EazyProvider();
 
-export default eazyProvider;
+// export default eazyProvider;
